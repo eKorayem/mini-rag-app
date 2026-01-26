b8c0d94af39f88db24cc61aa40b9e2f2ba029328
Eslam Atia <eslam.mo.atia@gmail.com>
2026-01-26 20:17:15 +0200

    Updated

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
-app = FastAPI()
+from dotenv import load_dotenv
+load_dotenv(".env")
 
-@app.get("/welcome")
-def welcome():
-    return {
-        "message" : "Hello"
-    }
+from routes import base
+
+
+app = FastAPI()
+app.include_router(base.base_router)
