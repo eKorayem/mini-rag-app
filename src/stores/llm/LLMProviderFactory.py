b6e9b5026822133302e2db17b580a82dee681e54
Eslam Atia <eslam.mo.atia@gmail.com>
2026-02-18 17:00:46 +0200

    Created LLMProviderFactory

---
+++ src/stores/llm/LLMProviderFactory.py
@@ -0,0 +1,27 @@
+from .LLMEnums import LLMEnums
+from .providers import OPENAIProvider, COHEREProvider
+
+class LLMProviderFactory:
+    def __init__(self, config: dict):
+        self.config = config
+    
+    def create(self, provider: str):
+        if provider == LLMEnums.OPENAI.value:
+            return OPENAIProvider(
+                api_key=self.config.OPENAI_API_KEY,
+                api_url=self.config.OPENAI_API_URL,
+                defualt_input_max_characters=self.config.INPUT_DAFAULT_MAX_CHARACTERS,
+                defualt_generation_max_out_tokens=self.config.GENERATION_DAFAULT_MAX_TOKENS,
+                default_generation_temperature=self.config.GENERATION_DAFAULT_TEMPERATURE
+            )
+        
+        elif provider == LLMEnums.COHERE.value:
+            return COHEREProvider(
+                api_key=self.config.COHERE_API_KEY,
+                defualt_input_max_characters=self.config.INPUT_DAFAULT_MAX_CHARACTERS, 
+                defualt_generation_max_out_tokens=self.config.GENERATION_DAFAULT_MAX_TOKENS,
+                default_generation_temperature=self.config.GENERATION_DAFAULT_TEMPERATURE
+            )
+        
+        else:
+            raise ValueError(f"Unsupported provider: {provider}")
